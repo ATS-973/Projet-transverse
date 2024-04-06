20991e970e8677d6d2cd9f8447c98fcb0c615b2c
amine <amine.frikha@yahoo.com>
2024-04-06 13:08:18 +0200

    moteur physique

---
+++ moteur_physique.py
@@ -0,0 +1,40 @@
+gravity = a #constante
+ground_thickness = 10
+mouse_trajectory = []
+
+class Object:
+
+    def __init__(self, x_pos, y_pos, radius, color, mass, retention, y_speed, x_speed, id, friction):
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.radius = radius  #pas nécessaire
+        self.color = color
+        self.mass = mass
+        self.retention = retention
+        self.y_speed = y_speed
+        self.x_speed = x_speed
+        self.id = id
+        self.friction = friction
+        self.selected = False
+
+
+
+    def update_pos(self, mouse):
+        if not self.selected:
+            self.y_pos += self.y_speed
+            self.x_pos += self.x_speed
+        else:
+            self.x_pos = mouse[0]
+            self.y_pos = mouse[1]
+
+
+
+def calc_motion_vector():
+    x_speed = 0
+    y_speed = 0
+    if len(mouse_trajectory) > 10:
+        x_speed = (mouse_trajectory[-1][0] - mouse_trajectory[0][0]) / len(mouse_trajectory)
+        y_speed = (mouse_trajectory[-1][1] - mouse_trajectory[0][1]) / len(mouse_trajectory)
+    return x_speed, y_speed
+
+
